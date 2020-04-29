19b5b8639dfe4ec9607c395c998bbb0bcc204831
orianisihaloho <orianisihaloho@hotmail.com>
2020-04-29 21:49:36 +0700

    Initial commit

---
+++ src/test/java/demo/steps/ToDoStepDefinition.java
@@ -0,0 +1,10 @@
+package demo.steps;
+
+import io.cucumber.java.en.Given;
+
+public class ToDoStepDefinition {
+
+    @Given("User is on to do list page")
+    public void userIsOnToDoListPage() {
+    }
+}
